5d0d9105cfec3428d0204b7e9b4dbe4b28de9c5d
Ashwani-Kumar-Rai <ashwani.profession@gmail.com>
2023-06-16 00:09:06 +0530

    initial commit

---
+++ flask/1.How to Make Websites with Python.py
@@ -0,0 +1,3 @@
+# flask is a micro web framework : for building websites with python
+# it is typically used as kind of a backend and then another front end is connected it to rest api  
+# THIS IS A MICROFRAMEWORK REATHER THEN A FULL FLEDGED FRAMEWORK .
